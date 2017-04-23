d5882cbd75955b54a3ef94a1cf8c987b4e88ca12
Stuart Drummond <stuartrdr@gmail.com>
2017-04-23 13:52:57 -0700

    Updates on video list + CSS applied

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,3 +1,23 @@
-/**
- * Created by stuar on 4/22/2017.
- */
+import React, {Component} from 'react'
+
+class VideoListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {video: props.video}
+    }
+
+    render() {
+        return (<li className="list-group-item">
+                   <div className="video-list media">
+                       <div className="media-left">
+                            <img className="media-object" src={this.state.video.snippet.thumbnails.default.url} />
+                       </div>
+                       <div className="media-body">
+                           <div className="media-body">{this.state.video.snippet.title}</div>
+                       </div>
+                   </div>
+               </li>);
+    }
+}
+
+export default VideoListItem;
